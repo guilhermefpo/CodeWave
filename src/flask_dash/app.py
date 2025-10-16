77cedf177cafc76b1d34886687208fc691245229
Hugo Leo <hugoleonardo040906@gmail.com>
2025-10-15 22:04:23 -0300

    feat:graficos

--- src/flask_dash/app.py
+++ src/flask_dash/app.py
@@ -1,26 +1,45 @@
 from flask import Flask, render_template, url_for
-import dash
-from dash_app import create_dash_apli
+import pandas as pd
+import numpy as np
+
+
+
+import os
+from grap import criando_grap
+
 
 
 app=Flask(__name__)
 
 
-@app.route("/home")
+@app.route("/")
 def home():
     return render_template("index.html")
 
 
 @app.route("/indicadores")
 def indicadores():
-    return render_template("principais_indicadores.html", )
+       print("Gerando gráficos...")
+       graficos = criando_grap()
+    
+       if graficos:
+         print("Gráficos gerados com sucesso!")
+         return render_template('principais_indicadores.html',
+           grap_etario=graficos['barras'],
+           grap_piramid=graficos['piramide'],
+           grap_setor=graficos['setor'],
+           grap_domi=graficos['domici']
+        )
+       else:
+         print("Erro ao gerar gráficos")
+         return render_template('erro.html')
+    
 
 
-create_dash_apli(app)
+
+
 
 
 
 if __name__=="__main__":
     app.run(debug=True)
-
-
